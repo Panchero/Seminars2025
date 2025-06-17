f980d8f4029bd6e40a061b1003691758a9989a24
Семен <marbledrop@yandex.ru>
2025-06-17 23:42:35 +0300

    Add Task 0001

---
+++ Task0001/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, 
+является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+*/
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("нет");
+
+// Console.WriteLine(Math.Pow (5, 3)); первое - число, которое хотим возвести, второе - степнь, в которую хотим возвести
